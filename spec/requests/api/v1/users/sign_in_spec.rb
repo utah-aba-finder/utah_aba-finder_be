d227fb2ad0b46b7ead26b127380a2c8b11f4d8d6
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-09-11 21:58:12 -0600

    TDD user sign up

---
+++ spec/requests/api/v1/users/sign_in_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe "User Sign-Up", type: :request do
+  it 'creates a new user and associates them with a provider' do
+    sign_up_params = {
+      user: {
+        email: 'new_provider@example.com',
+        password: 'password',
+        password_confirmation: 'password',
+        provider_name: 'Provider ABC'
+      }
+    }
+
+    post '/signup', params: sign_up_params
+
+    expect(response).to have_http_status(:created)
+
+    user = User.find_by(email: 'new_provider@example.com')
+    expect(user.provider_id).to eq(123)
+  end
+end
