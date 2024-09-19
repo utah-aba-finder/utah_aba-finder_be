1c567592a054ca281273c13ed9040dc55b4d252f
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-09-19 14:25:15 -0600

    small update

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :id, :email, :created_at
+  attributes :id, :email, :created_at, :provider_id
 
   attribute :created_date do |user|
     user.created_at && user.created_at.strftime('%m/%d/%Y')
