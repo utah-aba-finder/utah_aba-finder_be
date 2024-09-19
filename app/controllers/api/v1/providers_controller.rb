39797273c87037d711546942beea383d69987acd
Chee Lee <cheeleertr@gmail.com>
2024-09-18 23:03:36 -0500

    refactor show to authenticate user and match user provider_id and provider id request.

--- app/controllers/api/v1/providers_controller.rb
+++ app/controllers/api/v1/providers_controller.rb
@@ -1,11 +1,26 @@
 class Api::V1::ProvidersController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:show]
+  
   # to verify authentication is working, currently only functioning provider endpoint
   def index
     render json: UtahAbaFinderService.get_providers
   end
 
   def show
-    render json: UtahAbaFinderService.get_provider(params[:id])
+    if authenticate_user_provider_id
+      render json: UtahAbaFinderService.get_provider(params[:id])
+    else
+      render json: {error: "Unauthorized"}, status: :unauthorized
+    end
+  end
+
+  private
+
+  def authenticate_user_provider_id
+    if current_user && current_user.provider_id == params[:id].to_i
+      true
+    else
+      false
+    end
   end
 end
