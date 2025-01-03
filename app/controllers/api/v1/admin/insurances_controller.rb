e481aeec639dae70a5be35d8422813f4e50f0a13
Chee Lee <cheeleertr@gmail.com>
2025-01-02 20:42:48 -0600

    refactor service and controller for insurance crud, tests passing in postman

--- app/controllers/api/v1/admin/insurances_controller.rb
+++ app/controllers/api/v1/admin/insurances_controller.rb
@@ -2,16 +2,16 @@ class Api::V1::Admin::InsurancesController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_super_admin
 
-  def show
-      render json: UtahAbaFinderService.get_all_insurances(params[:id])
-  end
-
   def create
       render json: UtahAbaFinderService.create_insurance(params)
   end
 
   def update
       render json: UtahAbaFinderService.update_insurance(params[:id], params)
+  end
+
+  def destroy
+    render json: UtahAbaFinderService.delete_insurance(params[:id])
   end
 
   private
