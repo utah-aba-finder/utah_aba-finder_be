0c625d5b52fd037af10c173b7515796e9f081b61
Garrett Bowman <garrettbowman248@gmail.com>
2025-01-15 14:16:23 -0600

    Feat: Setup Devise for PW Reset

---
+++ app/controllers/users/passwords_controller.rb
@@ -0,0 +1,21 @@
+class Users::PasswordsController < Devise::PasswordsController
+  respond_to :json
+
+  def create
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    if successfully_sent?(resource)
+      render json: { status: 'ok', message: 'Reset password instructions sent' }
+    else
+      render json: { status: 'error', message: resource.errors.full_messages.join(', ') }
+    end
+  end
+
+  def update
+    self.resource = resource_class.reset_password_by_token(resource_params)
+    if resource.errors.empty?
+      render json: { status: 'ok', message: 'Password has been reset' }
+    else
+      render json: { status: 'error', message: resource.errors.full_messages.join(', ') }
+    end
+  end
+end
